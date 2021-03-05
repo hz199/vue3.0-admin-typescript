32e4a05f1cf56fff085faf293400b5916162cfd1
hz <15738818183@163.com>
2021-03-05 18:09:33 +0800

    feat: chart

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -9,15 +9,11 @@
           :color="item.color"
         ></NumberCard>
       </el-col>
-      <!-- <el-col :xs="24" :lg="6">
-        <NumberCard></NumberCard>
+    </el-row>
+    <el-row>
+      <el-col :xs="24" :lg="18">
+        <div id="echarts" style="width:500px;height:500px;"></div>
       </el-col>
-      <el-col :xs="24" :lg="6">
-        <NumberCard></NumberCard>
-      </el-col>
-      <el-col :xs="24" :lg="6">
-        <NumberCard></NumberCard>
-      </el-col> -->
     </el-row>
   </div>
 </template>
@@ -26,6 +22,15 @@
 import { defineComponent } from 'vue'
 import NumberCard from './NumberCard/index.vue'
 import homeMock from './homeMock'
+
+
+import * as echarts from 'echarts/core'
+import { ECBasicOption } from 'echarts/types/dist/shared'
+import { BarChart } from 'echarts/charts'
+import { GridComponent, TooltipComponent } from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
+echarts.use([BarChart, GridComponent, CanvasRenderer, TooltipComponent])
+
 
 export default defineComponent({
   name: 'Home',
@@ -36,6 +41,41 @@ export default defineComponent({
   },
   components: {
     NumberCard
+  },
+  mounted () {
+    const myChart = echarts.init(document.getElementById('echarts') as HTMLDivElement)
+    // 指定图表的配置项和数据
+    const option: ECBasicOption = {
+      baseOption: {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      }
+      // title: {
+      //     text: 'ECharts 入门示例'
+      // },
+      // tooltip: {},
+      // legend: {
+      //     data:['销量']
+      // },
+      // xAxis: {
+      //     data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+      // },
+      // yAxis: {},
+      // series: [{
+      //     name: '销量',
+      //     type: 'bar',
+      //     data: [5, 20, 36, 10, 10, 20]
+      // }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
   }
 })
 </script>
